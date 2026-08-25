a4c8b7149843ed4fc1c672b09e389375a59859d9
Albertine <albertine.gjos@orbitntnu.com>
2026-08-25 13:39:26 -0300

    Added main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from load_data import load_data_set
 from clean_data import clean_data_set
 from data_preparation import prepare_data_set
+from select_features import select_features
 from linear_regression import cross_validation_linear_regression
 from polynomial_regression import cross_validation_polynomial_regression
 
 data_set = load_data_set()
 data_set = clean_data_set(data_set)
 data_set = prepare_data_set(data_set)
+data_set = select_features(data_set, features)  # Select features based on correlation and domain knowledge
 
 train = data_set.sample(frac=0.8, random_state=42)
 test = data_set.drop(train.index)
